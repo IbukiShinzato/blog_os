899bcafc70a06981af4d6a43fc54455d956e7a46
IbukiShinzato <e235718@ie.u-ryukyu.ac.jp>
2026-02-23 21:08:50 +0900

    feat: ベアメタルでのコンパイルまで

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+// mangleするとユニークなシンボルになるのでno_mangle
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic_handler(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
