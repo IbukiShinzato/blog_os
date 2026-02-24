c2307b6e69a051fd92406c09cb3fc94b9a4e9722
IbukiShinzato <e235718@ie.u-ryukyu.ac.jp>
2026-02-24 23:40:37 +0900

    feat: ターゲット指定のjson作成、VGAテキストバッファでの文字出力

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,32 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello!";
+static BLOG_OS: &[u8] = b"Blog OS!";
+
+// １行に160文字入る
+static ENTER: isize = 160;
+
 // mangleするとユニークなシンボルになるのでno_mangle
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            // 1バイト目が文字、2バイト目が背景の色
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xd;
+        }
+    }
+
+    for (i, &byte) in BLOG_OS.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2 + ENTER) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1 + ENTER) = 0xb;
+        }
+    }
+
     loop {}
 }
 
@@ -13,4 +36,3 @@ pub extern "C" fn _start() -> ! {
 fn panic_handler(_info: &PanicInfo) -> ! {
     loop {}
 }
-
