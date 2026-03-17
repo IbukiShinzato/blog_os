91eade2ef6f85085cef4dc2938053eedef680909
IbukiShinzato <e235718@ie.u-ryukyu.ac.jp>
2026-03-17 21:51:38 +0900

    feat: Writer可変静的変数を作成して、外部からVGAテキストバッファの書き込み可能にする

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,16 @@ mod vga_buffer;
 
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello!";
-static BLOG_OS: &[u8] = b"Blog OS!";
-
-// 80(文字 + 背景色) = 160Byte
-// １行に160文字入る
-static ENTER: isize = 160;
-
 // mangleするとユニークなシンボルになるのでno_mangle
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
-
-    loop {}
+    println!("Hello, World{}", '!');
+    panic!("Some panic message");
+    // loop {}
 }
 
 #[panic_handler]
-fn panic_handler(_info: &PanicInfo) -> ! {
+fn panic_handler(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,17 @@
+use core::fmt;
+use lazy_static::lazy_static;
+use spin::Mutex;
+use volatile::Volatile;
+
+// どのファイルからでもこの変数にアクセスすることが可能
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::LightCyan, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
+    });
+}
+
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 // 各要素をu8(1Byte)に指定
@@ -46,7 +60,7 @@ const BUFFER_WIDTH: usize = 80;
 // ここに文字を入れて出力する
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
 // 画面を書き出す為の型
@@ -72,18 +86,39 @@ impl Writer {
                 let col = self.column_position;
 
                 // Bufferに直接ScreenChar構造体を入れ込む
+                // volatileにすることで、コンパイラの最適化を防ぐ
                 let color_code = self.color_code;
-                self.buffer.chars[row][col] = ScreenChar {
+                self.buffer.chars[row][col].write(ScreenChar {
                     ascii_character: byte,
                     color_code,
-                };
+                });
                 self.column_position += 1;
             }
         }
     }
 
+    // 改行
     fn new_line(&mut self) {
-        unimplemented!()
+        (1..BUFFER_HEIGHT).for_each(|row| {
+            (0..BUFFER_WIDTH).for_each(|col| {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            })
+        });
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    // 指定した行を削除
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+
+        self.buffer.chars[row]
+            .iter_mut()
+            .for_each(|char| char.write(blank));
     }
 
     pub fn write_string(&mut self, s: &str) {
@@ -98,15 +133,27 @@ impl Writer {
     }
 }
 
-pub fn print_something() {
-    let mut writer = Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::LightCyan, Color::Black),
-        //  VGAテキストバッファへのメモリマップドI/O (MMIO) アドレス
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
+// write!とwriteln!マクロを使用できるようにする
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
 
-    writer.write_byte(b'H');
-    writer.write_string("ello ");
-    writer.write_string("Wörld!");
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
 }
